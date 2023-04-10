a45d41d018d784fbaa2ea95f5721727792b25441
Monalisa Ganguly <monalisaganguly620@gmail.com>
2023-04-10 13:05:05 +0530

    working

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 var app=angular.module("myMod",[]);
 
- app.controller("myController", function($scope){                //$scope is  model here .
+ app.controller("myController", function($scope,$location, $interval){                //$scope is provide model .
     $scope.name = "Monalisa Ganguly"
     $scope.number=0
     $scope.myFunction= function(){
@@ -50,6 +50,34 @@ var app=angular.module("myMod",[]);
         {name:"Anita",city:"Burdwan",age:46},
         {name:"Rupam",city:"Burdwan",age:28},
     ];
+
+    $scope.abc= [
+        {name:"Mona",gender:"Female", class:12},
+        {name:"Raj",gender:"Male", class:10},
+        {name:"Munna",gender:"Male", class:11},
+        {name:"Anita",gender:"Female", class:9},
+        {name:"Rupam",gender:"Male", class:7}
+        
+    
+    ];
+    $scope.EmpView="index2.html";
+$scope.url=$location.absUrl();
+$scope.protocol=$location.protocol();
+$scope.host=$location.host();
+$scope.port=$location.port();
+
+// $scope.count=0;
+// $interval(function(){
+//     $scope.count++
+// },2000 )
+
+$interval(function(){
+    $scope.dateAndTime=new Date();
+},1000)
+
+
+
+
 
 });
 
